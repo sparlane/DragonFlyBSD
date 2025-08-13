2a72bc4634634c2c31741466fa76ca1cd1cb62af
Scott Parlane <scott@parlanenz.com>
2025-08-13 22:31:34 +1200

    gcc90: apply changes from gcc80
    bd23261bc0 libstdc++: Fix unsigned wraparound in codecvt::do_length [PR105857]
    fca8e87b2e gcc: Define ElfW() if it is not defined.
    2007c584a7 gcc8: Fix the fix.
    9cf0c62db7 gccs had ElfW() already. Comment out for now.
    2ecd6c6623 gcc: Allow our C++ headers to survive -Wsystem-headers (2).
    894c19b2d8 gcc: Allow our C++ headers to survive -Wsystem-headers.
    de2610f57c Adjust makefiles for GCC80 update.
    df642abc04 gcc80: Add local -save-temps=objects option.
    a69def156b gcc80: Handle TZ specific "%+" format in strftime.
    fcad8200ea gcc80: Remove throw() from protos in libstdc++ headers.
    39964fd19e gcc80: Include <stdlib.h> for alloca() prototype.
    3dcb3c95ea gcc80: Add alternative variant of pthread presence checking.
    632577dbc0 gcc80: Adjust search paths for base compiler layout.
    ed36d35d08 gcc80: Adjust default dynamic linker search location.

--- contrib/gcc-9/gcc/gcc.c
+++ contrib/gcc-9/gcc/gcc.c
@@ -271,7 +271,8 @@ static const char *target_sysroot_hdrs_suffix = 0;
 static enum save_temps {
   SAVE_TEMPS_NONE,		/* no -save-temps */
   SAVE_TEMPS_CWD,		/* -save-temps in current directory */
-  SAVE_TEMPS_OBJ		/* -save-temps in object directory */
+  SAVE_TEMPS_OBJ,		/* -save-temps in object directory */
+  SAVE_TEMPS_OBJZ		/* -save-temps in object directory with mangling */
 } save_temps_flag;
 
 /* Output file to use to get the object directory for -save-temps=obj  */
@@ -2620,6 +2621,7 @@ for_each_path (const struct path_prefix *paths,
 	  len = strlen (pl->prefix);
 	  memcpy (path, pl->prefix, len);
 
+#if 0  /* DragonFly base: MACHINE/VERSION isn't used anywhere */
 	  /* Look first in MACHINE/VERSION subdirectory.  */
 	  if (!skip_multi_dir)
 	    {
@@ -2628,6 +2630,7 @@ for_each_path (const struct path_prefix *paths,
 	      if (ret)
 		break;
 	    }
+#endif
 
 	  /* Some paths are tried with just the machine (ie. target)
 	     subdir.  This is used for finding as, ld, etc.  */
@@ -4147,6 +4150,8 @@ driver_handle_option (struct gcc_options *opts,
       else if (strcmp (arg, "obj") == 0
 	       || strcmp (arg, "object") == 0)
 	save_temps_flag = SAVE_TEMPS_OBJ;
+      else if (strcmp (arg, "objects") == 0)
+        save_temps_flag = SAVE_TEMPS_OBJZ;
       else
 	fatal_error (input_location, "%qs is an unknown %<-save-temps%> option",
 		     decoded->orig_option_with_args_text);
@@ -4374,6 +4379,7 @@ process_command (unsigned int decoded_options_count,
   /* FIXME: make_relative_prefix doesn't yet work for VMS.  */
   if (!gcc_exec_prefix)
     {
+#if 0  /* DragonFly base: Never use relative prefix (not bootstrapped) */
       gcc_exec_prefix = get_relative_prefix (decoded_options[0].arg,
 					     standard_bindir_prefix,
 					     standard_exec_prefix);
@@ -4382,6 +4388,7 @@ process_command (unsigned int decoded_options_count,
 					     standard_libexec_prefix);
       if (gcc_exec_prefix)
 	xputenv (concat ("GCC_EXEC_PREFIX=", gcc_exec_prefix, NULL));
+#endif
     }
   else
     {
@@ -4426,11 +4433,13 @@ process_command (unsigned int decoded_options_count,
 	    len -= sizeof ("/lib/gcc/") - 1;
 	}
 
+#if 0  /* DragonFly base: Bad Paths */
       set_std_prefix (gcc_exec_prefix, len);
       add_prefix (&exec_prefixes, gcc_libexec_prefix, "GCC",
 		  PREFIX_PRIORITY_LAST, 0, 0);
       add_prefix (&startfile_prefixes, gcc_exec_prefix, "GCC",
 		  PREFIX_PRIORITY_LAST, 0, 0);
+#endif
     }
 
   /* COMPILER_PATH and LIBRARY_PATH have values
@@ -4641,6 +4650,10 @@ process_command (unsigned int decoded_options_count,
 	}
 
     }
+  else if (save_temps_flag == SAVE_TEMPS_OBJZ && save_temps_prefix != NULL)
+    {
+      save_temps_length = strlen (save_temps_prefix);
+    }
   else if (save_temps_prefix != NULL)
     {
       free (save_temps_prefix);
@@ -4686,6 +4699,11 @@ process_command (unsigned int decoded_options_count,
   if (!gcc_exec_prefix)
     {
 #ifndef OS2
+#if 1  /* DragonFly base: All tools in the same common location */
+      add_prefix (&exec_prefixes, standard_libexec_prefix, NULL,
+                 PREFIX_PRIORITY_LAST, 0, 0);
+#endif
+#if 0  /* DragonFly base: Bad paths */
       add_prefix (&exec_prefixes, standard_libexec_prefix, "GCC",
 		  PREFIX_PRIORITY_LAST, 1, 0);
       add_prefix (&exec_prefixes, standard_libexec_prefix, "BINUTILS",
@@ -4693,8 +4711,11 @@ process_command (unsigned int decoded_options_count,
       add_prefix (&exec_prefixes, standard_exec_prefix, "BINUTILS",
 		  PREFIX_PRIORITY_LAST, 2, 0);
 #endif
+#endif
+#if 0  /* DragonFly base: Bad paths */
       add_prefix (&startfile_prefixes, standard_exec_prefix, "BINUTILS",
 		  PREFIX_PRIORITY_LAST, 1, 0);
+#endif
     }
 
   gcc_assert (!IS_ABSOLUTE_PATH (tooldir_base_prefix));
@@ -4709,12 +4730,14 @@ process_command (unsigned int decoded_options_count,
 	      accel_dir_suffix, dir_separator_str, tooldir_prefix2, NULL);
   free (tooldir_prefix2);
 
+#if 0  /* DragonFly base: Bad paths */
   add_prefix (&exec_prefixes,
 	      concat (tooldir_prefix, "bin", dir_separator_str, NULL),
 	      "BINUTILS", PREFIX_PRIORITY_LAST, 0, 0);
   add_prefix (&startfile_prefixes,
 	      concat (tooldir_prefix, "lib", dir_separator_str, NULL),
 	      "BINUTILS", PREFIX_PRIORITY_LAST, 0, 1);
+#endif
   free (tooldir_prefix);
 
 #if defined(TARGET_SYSTEM_ROOT_RELOCATABLE) && !defined(VMS)
