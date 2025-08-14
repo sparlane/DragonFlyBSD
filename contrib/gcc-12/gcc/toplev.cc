15fde3dd7f0e1ec139b3c7f948c04e360bd15f90
Scott Parlane <scott@parlanenz.com>
2025-08-14 18:43:04 +1200

    cc120: Fix build issue for options
    The field in cl_options isn't big enough to actually hold 1 << 63,
    so make it unsigned long long, and then replace the one place where
    a negative value is used to avoid the resulting error.

--- contrib/gcc-12/gcc/toplev.cc
+++ contrib/gcc-12/gcc/toplev.cc
@@ -1479,7 +1479,7 @@ process_options (bool no_backend)
 	   && dwarf2out_as_loc_support
 	   && dwarf2out_as_locview_support);
     }
-  else if (debug_variable_location_views == -1 && dwarf_version != 5)
+  else if (debug_variable_location_views == 2 && dwarf_version != 5)
     {
       warning_at (UNKNOWN_LOCATION, 0,
 		  "without %<-gdwarf-5%>, "
